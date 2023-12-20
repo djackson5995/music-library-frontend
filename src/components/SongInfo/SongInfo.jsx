110f90c1bf5c5282595830f88d2447ce9b5d55de
djackson5995 <146674106+djackson5995@users.noreply.github.com>
2023-12-20 00:08:34 -0700

    added more styling

--- src/components/SongInfo/SongInfo.jsx
+++ src/components/SongInfo/SongInfo.jsx
@@ -5,21 +5,29 @@ const SongInfo = ({ songObj }) => {
     songObj && (
       <div className="flex-item">
         <h4>{songObj.title}</h4>
-        <div className="data-item">
-          <span>Artist</span>
-          <span>{songObj.artist}</span>
+        <div>
+          <div className="data-item">
+            <span>Artist</span>
+            <span>{songObj.artist}</span>
+          </div>
         </div>
-        <div className="data-item">
-          <span>Genre</span>
-          <span>{songObj.genre}</span>
+        <div>
+          <div className="data-item">
+            <span>Genre</span>
+            <span>{songObj.genre}</span>
+          </div>
         </div>
-        <div className="data-item">
-          <span>Album</span>
-          <span>{songObj.album}</span>
+        <div>
+          <div className="data-item">
+            <span>Album</span>
+            <span>{songObj.album}</span>
+          </div>
         </div>
-        <div className="data-item">
-          <span>Release Date </span>
-          <span>{songObj.releaseDate}</span>
+        <div>
+          <div className="data-item">
+            <span>Release Date </span>
+            <span>{songObj.releaseDate}</span>
+          </div>
         </div>
       </div>
     )
